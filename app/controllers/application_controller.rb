cd63d5ff58ae171644e9b9e161235adc706a4322
MKNJ1961 <skillcrush1961@gmail.com>
2020-05-16 10:18:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,64 @@
+# require "./config/environment"
+# require "./app/models/user"
+# class ApplicationController < Sinatra::Base
+
+#   configure do
+#     set :views, "app/views"
+#     enable :sessions
+#     set :session_secret, "password_security"
+#   end
+
+#   get "/" do
+#     erb :index
+#   end
+
+#   get "/signup" do
+#     erb :signup
+#   end
+
+#   post "/signup" do
+#     #your code here
+
+#   end
+
+#   get '/account' do
+#     @user = User.find(session[:user_id])
+#     erb :account
+#   end
+
+
+#   get "/login" do
+#     erb :login
+#   end
+
+#   post "/login" do
+#     ##your code here
+#   end
+
+#   get "/failure" do
+#     erb :failure
+#   end
+
+#   get "/logout" do
+#     session.clear
+#     redirect "/"
+#   end
+
+#   helpers do
+#     def logged_in?
+#       !!session[:user_id]
+#     end
+
+#     def current_user
+#       User.find(session[:user_id])
+#     end
+#   end
+
+# end
+
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -11,28 +70,60 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
+  #just renders the homepage
 
   get "/signup" do
     erb :signup
   end
+  #renders or shows the signup page, where user can input data regarding their signup
 
   post "/signup" do
-    #your code here
+    if params[:username]=="" || params[:password]==""
+      redirect to '/failure'
+    end
+    #redirect to failure if they enter bad data
 
+    user = User.new(:username => params[:username], :password => params[:password])
+    #create a user with the parameters they enter
+    
+    if user.save
+			redirect to "/login"
+		else
+			redirect to "/failure"
+		end
+		#if they save to database, take them to the homepage
   end
 
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
   end
+  #show their account page, and show the correct account page by finding the user based on their session id 
 
 
   get "/login" do
     erb :login
   end
+  #show the login page
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/account"
+		else
+			redirect to "/failure"
+		end
+  end
+  #process their login information, and authenticate their password. 
+
+  get "/success" do
+    if logged_in?
+      erb :success
+    else
+      redirect to "/login"
+    end
   end
 
   get "/failure" do
@@ -41,7 +132,7 @@ class ApplicationController < Sinatra::Base
 
   get "/logout" do
     session.clear
-    redirect "/"
+    redirect to "/"
   end
 
   helpers do
@@ -55,3 +146,4 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+
